555bbca2e7365946e26f6213dd9f747c92b1cda4
Vikas Sharma <70948583+vikasbashu@users.noreply.github.com>
2023-10-09 16:55:02 +0530

    added login boilerplate

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { FirebaseProvider } from './context/Firebase';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FirebaseProvider >
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </FirebaseProvider>
   </React.StrictMode>
 );
